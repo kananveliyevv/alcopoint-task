785c270dff5d7b480cceac65262a8f68854c0667
kananveliyevv <kanan.veliyev@gmail.com>
2026-05-26 23:50:02 +0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,5 +0,0 @@
-// Placeholder file.
-// This project was originally Firebase-based, but in this build the sync layer uses Supabase.
-// Keeping this file prevents 404 errors from the <script src="firebase-config.js"> tag.
-globalThis.firebaseConfig = globalThis.firebaseConfig || {};
-
