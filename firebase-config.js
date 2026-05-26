8f06d1e4b6d9d8183fd7a3863a3822d8d03f672a
kananveliyevv <kanan.veliyev@gmail.com>
2026-05-26 22:33:22 +0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCNCVcl7eqaKNdU2757efzZ2mHkQ5qLIBU",
-  authDomain: "alcopoint-task.firebaseapp.com",
-  databaseURL: "https://alcopoint-task-default-rtdb.firebaseio.com",
-  projectId: "alcopoint-task",
-  storageBucket: "alcopoint-task.firebasestorage.app",
-  messagingSenderId: "647760009556",
-  appId: "1:647760009556:web:0b5a67d0e59859e33d90c1"
-};
