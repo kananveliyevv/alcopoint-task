692ce972a00ff0cf273269307bc4dd3cba240a8a
kananveliyevv <kanan.veliyev@gmail.com>
2026-05-26 22:34:54 +0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+"https://eferozgkqhhkdgekqlhh.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_Hy769-TrtR1SZDaNNuSRRw_RMfl_NuD";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
